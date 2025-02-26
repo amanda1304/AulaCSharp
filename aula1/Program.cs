4b11333dd404469e3052bed3b603d42075953a11
amanda1304 <mand.amanda1304@gmail.com>
2025-02-26 14:50:06 -0300

    Upload da primeiro aula

---
+++ aula1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace aula1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade = 26;
+            string nomeCompleto = "Amanda Alves de França";
+           // Console.WriteLine(nomeCompleto);
+            var variavelSemTipo = "Amanda";
+            Console.WriteLine(variavelSemTipo.GetType());
+            Console.WriteLine("Digite o seu nome: ");
+            string nomeUsuario = Console.ReadLine();
+
+            Console.WriteLine("Digite a sua idade: ");
+            string idadeUsuario = Console.ReadLine();
+            
+            Console.WriteLine($"Meu nome é {nomeUsuario} e a minha idade é {idadeUsuario}");
+            
+        }
+    }
+}
